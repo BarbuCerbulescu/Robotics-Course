1d3b494d9e861e2f3832bcaf4a2baba5214966ee
BarbuCerbulescu <56958401+BarbuCerbulescu@users.noreply.github.com>
2019-11-11 18:31:42 +0200

    Update RGB_led.ino

--- Lab Homeworks/RGB_led.ino
+++ Lab Homeworks/RGB_led.ino
@@ -1,4 +1,4 @@
-int potPin1 = A0;
+int potPin1=A0;
 int potPin2=A2;
 int potPin3=A4;    
 int rPin =9;
@@ -6,7 +6,7 @@ int bPin=10;
 int gPin=11;     
 
 void setup() {
-  pinMode(rPin, OUTPUT);
+  pinMode(rPin,OUTPUT);
   pinMode(bPin,OUTPUT);
   pinMode(gPin,OUTPUT);
   Serial.begin(9600);
@@ -17,17 +17,20 @@ void loop() {
  Serial.print("Red Value=");
  Serial.print(potVal1 /4);
  Serial.print("\n");
-  int potVal2=analogRead(potPin2);
-  Serial.print("Blue Value=");
+  
+ int potVal2=analogRead(potPin2);
+ Serial.print("Blue Value=");
  Serial.print(potVal2 /4);
  Serial.print("\n");
-  int potVal3=analogRead(potPin3);
-  Serial.print("Green Value=");
+  
+ int potVal3=analogRead(potPin3);
+ Serial.print("Green Value=");
  Serial.print(potVal3 /4);
  Serial.print("\n");
+  
  Serial.println();
  
-   analogWrite(rPin,potVal1 /4);
-   analogWrite(bPin,potVal2 /4);
-   analogWrite(gPin,potVal3 /4);
+ analogWrite(rPin,potVal1 /4);
+ analogWrite(bPin,potVal2 /4);
+ analogWrite(gPin,potVal3 /4);
 }
